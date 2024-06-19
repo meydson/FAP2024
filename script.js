3a281aa6bcc28bd33ff8cf21e4724de70ab5b09d
Meydson Baracho <meydsonbaracho@gmail.com>
2024-06-19 19:19:56 -0300

    Exercicio For e While

---
+++ script.js
@@ -0,0 +1,13 @@
+function teste_for() {
+    for (i = 100; i > 0; i--) {
+        document.write(i, "<br>");
+    }
+}
+
+function teste_while() {
+    var i = 1000;
+    while (i > 900) {
+        document.write(i, "<br>");
+        i--;
+    }
+}

51586a86798e9f689da495bfbaab9e5fa571cd55
Meydson Baracho <64757536+meydson@users.noreply.github.com>
2024-06-24 16:38:14 -0300

    Segunda lista

---
+++ lista2.js
@@ -0,0 +1,14 @@
+function exercicio1() {
+    document.write("Hello world!");
+}
+
+function exercicio2() {
+    var numero = parseInt(prompt("Digite um valor!"));
+    document.write(`O cubo do número ${numero} é ${numero * numero * numero}`);
+}
+
+function exercicio4() {
+    var base = parseInt(prompt("Informe o valor da base do triângulo!"));
+    var altura = parseInt(prompt("informe o valor da altura do triângulo!"));
+    document.write(`O valor da área do triângulo informado é ${(base*altura)/2}`);
+}
